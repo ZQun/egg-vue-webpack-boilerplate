5260f087aefc3a59040e8f79adf4d6c4949ca75b
dodoT2 <q_un@foxmail.com>
2017-11-24 00:33:22 +0800

    demo

---
+++ app/web/component/app/demo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>
+            Hello World!!!!
+        </h1>
+    </div>
+</template>
+
+<script type="text/babel">
+    export default {
+        computed: {
+
+        }
+    }
+</script>
+
+
+<style>
+
+</style>
--- app/web/component/app/router.js
+++ app/web/component/app/router.js
@@ -4,26 +4,32 @@ import VueRouter from 'vue-router';
 
 import ListView from './list';
 import DetailView from './detail';
+// demo
+// import demoTest from './demo.vue';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: 'history',
-  base: '/app',
-  routes: [
-    {
-      path: '/',
-      component: ListView
-    },
-    {
-      path: '/list',
-      component: ListView
-    },
-    {
-      path: '/detail/:id',
-      component: DetailView
-    }
-  ]
+    mode: 'history',
+    base: '/app',
+    routes: [
+        {
+            path: '/',
+            components: {
+                default: ListView,
+                demo: () => import('./demo.vue')
+                // demo: demoTest
+            }
+        },
+        {
+            path: '/list',
+            component: ListView
+        },
+        {
+            path: '/detail/:id',
+            component: DetailView
+        }
+    ]
 });
 
 export default router;
